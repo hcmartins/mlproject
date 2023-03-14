09cfcb39cfd82d710b14b442ba77704c88372107
Herbert Caxton-Martins <herbert.caxton-martins@panintelligence.com>
2023-03-14 10:28:22 +0000

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -26,5 +26,5 @@ class CustomException(Exception):
 #     try:
 #         a=1/0
 #     except Exception as e:
-#         logging.info("Divided by 0 error") 
+#         logging.info("Divide by Zero error") 
 #         raise CustomException(e, sys)
